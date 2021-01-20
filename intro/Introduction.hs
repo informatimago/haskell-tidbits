36ac10619f2a87a203042cd8048df50aefdbb246
André Thieme <ath@users.noreply.github.com>
2021-01-20 18:14:29 +0100

    Update Introduction.hs
    Updated indentation, removed bad `do` and `in`.

--- intro/Introduction.hs
+++ intro/Introduction.hs
@@ -1,21 +1,18 @@
 
 main :: IO ()
 main = do
- let a=[2,4..100]
-     b=drop 5 a
-   in
-   do
-   putStr "a = {"
-   mapM_ (\x -> do
-             putStr (show x)
-             putStr ", ") a
-   putStrLn "};"
+  let a=[2,4..100]
+      b=drop 5 a
+  putStr "a = {"
+  mapM_ (\x -> do
+    putStr (show x)
+    putStr ", ") a
+  putStrLn "};"
 
-   putStr "b = {"
-   mapM_ (\x -> do
-             putStr (show x)
-             putStr ", ") b
-   putStrLn "};"
-    
-   return ()
-
+  putStr "b = {"
+  mapM_ (\x -> do
+    putStr (show x)
+    putStr ", ") b
+  putStrLn "};"
+  
+  return ()
