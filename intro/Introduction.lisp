4a944fee4eb6335aa9efacf4c637e18a358409e6
Pascal J. Bourguignon <pjb@informatimago.com>
2021-01-20 19:22:19 +0100

    Renamed print-comma-list.

--- intro/Introduction.lisp
+++ intro/Introduction.lisp
@@ -1,10 +1,10 @@
-(defun printCommaList (name list)
+(defun print-comma-list (name list)
   (format t "~(~A~) = {~{~A~^, ~}};~%" name list))
 
 (defun main ()
   (let* ((a (loop :for n :from 2 :to 100 :by 2
                  :collect n))
          (b (subseq a 5)))
-    (printCommaList 'a a)
-    (printCommaList 'b b)
+    (print-comma-list 'a a)
+    (print-comma-list 'b b)
     (values)))
