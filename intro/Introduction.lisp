17ce1efb4e4fd1ebde5514a148fd38033c053765
Pascal J. Bourguignon <pjb@informatimago.com>
2021-01-20 19:21:09 +0100

    Factored out printCommaList.

--- intro/Introduction.lisp
+++ intro/Introduction.lisp
@@ -1,8 +1,10 @@
+(defun printCommaList (name list)
+  (format t "~(~A~) = {~{~A~^, ~}};~%" name list))
 
 (defun main ()
   (let* ((a (loop :for n :from 2 :to 100 :by 2
                  :collect n))
          (b (subseq a 5)))
-    (format t "~(~A~) = {~{~A~^, ~}};~%" 'a a)
-    (format t "~(~A~) = {~{~A~^, ~}};~%" 'b b)
+    (printCommaList 'a a)
+    (printCommaList 'b b)
     (values)))
